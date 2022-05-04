5c14e41fa78a350bb46ed0793a0c134758df3ef2
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-05-04 11:23:00 -0400

    commit tree be8ec9cc49202b5b5d93b2a9ce7f1cb72448f8cb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:item_id])
+    else
+      null
+    end
+    render json: item, include: :user
+  end
+
+  private
+
+  def render_not_found
+    render json: { error: 'User not found' }, status: :not_found
+  end
 end
